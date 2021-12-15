3c1d6ea54c99bdc6785761b396f0af8f0f1d8d5a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2021-12-15 17:29:05 +0530

    Postorder traversal.

---
+++ BinaryTree/Postorder traversal.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<sstream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int dt){
+        data=dt;
+        left=NULL;
+        right=NULL;
+    }
+    Node(){
+        left=NULL;
+        right=NULL;
+    }
+};
+
+Node * buildTree(string str){
+    if(str.length()==0 || str[0]=='N')
+        return NULL;
+    
+    vector<string> ip;
+    istringstream strm(str);
+    for(string str;strm>>str;)
+        ip.push_back(str);
+    
+    Node *root=new Node(stoi(ip[0]));
+    queue<Node*>q;
+    q.push(root);
+    int i=1;
+    while(!q.empty() && i<ip.size()){
+        Node *temp=q.front();
+        q.pop();
+        if(ip[i]!="N"){
+            temp->left=new Node(stoi(ip[i]));
+            q.push(temp->left);
+        }
+        i++;
+        if(i>=ip.size())
+            break;
+        if(ip[i]!="N"){
+            temp->right=new Node(stoi(ip[i]));
+            q.push(temp->right);
+        }
+        i++;
+    }
+
+    return root;
+    
+}
+
+
+
+// Solution class
+
+class Solution{
+    vector<int>res;
+public:
+
+// Function to traverse a tree inOrderly (recursively)
+
+    vector<int>inOrder(Node *root){
+        if(root){
+            inOrder(root->left);
+            inOrder(root->right);
+            res.push_back(root->data);
+        }
+        return res;
+    }
+};
+
+
+
+int main(){
+    string str;
+    getline(cin,str);
+    Node *root=buildTree(str);
+    Solution obj;
+    vector<int>v=obj.inOrder(root);
+    for(auto i:v) cout<<i<<" ";
+    return 0;
+}
