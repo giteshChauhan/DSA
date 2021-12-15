2278e3cfad0cd1f9b06f925bbb7c8768322c080f
Gitesh Chauhan <giteshchauhan20@gmail.com>
2021-12-15 23:00:46 +0530

    Inorder traversal (iterative).

---
+++ BinaryTree/Inorder traversal (iterative).cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<sstream>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int dt){
+        data=dt;
+        left=NULL;
+        right=NULL;
+    }
+    Node(){
+        left=NULL;
+        right=NULL;
+    }
+};
+
+Node * buildTree(string str){
+    if(str.length()==0 || str[0]=='N')
+        return NULL;
+    
+    vector<string> ip;
+    istringstream strm(str);
+    for(string str;strm>>str;)
+        ip.push_back(str);
+    
+    Node *root=new Node(stoi(ip[0]));
+    queue<Node*>q;
+    q.push(root);
+    int i=1;
+    while(!q.empty() && i<ip.size()){
+        Node *temp=q.front();
+        q.pop();
+        if(ip[i]!="N"){
+            temp->left=new Node(stoi(ip[i]));
+            q.push(temp->left);
+        }
+        i++;
+        if(i>=ip.size())
+            break;
+        if(ip[i]!="N"){
+            temp->right=new Node(stoi(ip[i]));
+            q.push(temp->right);
+        }
+        i++;
+    }
+
+    return root;
+    
+}
+
+
+
+// Solution class
+
+class Solution{
+public:
+
+// Function to traverse a tree inOrderly (iterative).
+
+    vector<int>inOrder(Node *root){
+        vector<int>res;
+        stack<Node*>st;
+        while(root || !st.empty()){
+            while(root){
+                st.push(root);
+                root=root->left;
+            }
+            root=st.top();
+            st.pop();
+            res.push_back(root->data);
+            root=root->right;
+        }
+
+        return res;
+    }
+
+};
+
+
+
+int main(){
+    string str;
+    getline(cin,str);
+    Node *root=buildTree(str);
+    Solution obj;
+    vector<int>v=obj.inOrder(root);
+    for(auto i:v) cout<<i<<" ";
+    return 0;
+}
+
