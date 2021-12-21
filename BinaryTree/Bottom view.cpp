bfe97522227ee39a7e2cfcd7e337396beb0d1ca0
Gitesh Chauhan <giteshchauhan20@gmail.com>
2021-12-21 15:05:15 +0530

    Bottom view.

---
+++ BinaryTree/Bottom view.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<queue>
+#include<map>
+#include<vector>
+#include<string>
+#include<sstream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int dt){
+        data=dt;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+vector<int>bottomView(Node*);
+
+Node *buildTree(string s){
+    if(s.length() == 0 || s[0]=='N')
+        return NULL;
+
+    vector<string>v;
+    istringstream strm(s);
+    for(string str;strm>>str;)
+        v.push_back(str);
+
+    Node *root=new Node(stoi(v[0]));
+    queue<Node*>q;
+    q.push(root);
+    int i=1;
+    while(!q.empty() && i < v.size()){
+        Node *temp=q.front();
+        q.pop();
+        if(v[i] != "N"){
+            temp->left=new Node(stoi(v[i]));
+            q.push(temp->left);
+        }
+        i++;
+        if(i >= v.size())
+            break;
+        if(v[i] != "N"){
+            temp->right=new Node(stoi(v[i]));
+            q.push(temp->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    Node *root=buildTree(s);
+    vector<int>bottom=bottomView(root);
+    for(auto i:bottom) cout<<i<<" ";
+    return 0;
+}
+
+
+// Function to find the bottom view of the given binary tree
+
+vector<int>bottomView(Node *root){
+
+    map<int,int>m;
+    queue<pair<Node*,int>>q;
+    q.push({root,0});
+
+    while(!q.empty()){
+        Node *currNode=q.front().first;
+        int h=q.front().second;
+        q.pop();
+
+        if(currNode->left)  q.push({currNode->left,h-1});
+        if(currNode->right) q.push({currNode->right,h+1});
+
+        m[h]=currNode->data;
+    }
+
+    vector<int>res;
+    for(auto it:m)
+        res.push_back(it.second);
+
+    return res;
+}
