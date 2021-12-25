dc3a94843b511ddceb9da06a514e8a7da0cfddef
Gitesh Chauhan <giteshchauhan20@gmail.com>
2021-12-25 23:41:58 +0530

    1st non-repeat. charac in stream of charac (func).

---
+++ LinkedList/First non-repeating character from the stream of characters.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+// Function to find the first non-repeating character from the stream of characters.
+// Here queue approach is used rather we can also use Doubly linked list approach.
+
+/*
+    Given an input stream of A of n characters consisting only of lower case alphabets.
+    The task is to find the first non repeating character, each time a character is inserted to the stream.
+    If there is no such character then append '#' to the answer.
+*/
+
+string firstNonRepeating(string a){
+
+    int arr[26]={0};
+    queue<int>q;
+    for(int i=0;i<a.length();i++){
+        arr[a[i]-'a']++;
+        if(arr[a[i]-'a'] == 1)
+            q.push(a[i]);
+        
+        while(!q.empty() && arr[q.front()-'a'] != 1)
+            q.pop();
+        
+        if(q.empty())
+            a[i]='#';
+        else
+            a[i]=q.front();
+    }
+
+    return a;
+}
+
