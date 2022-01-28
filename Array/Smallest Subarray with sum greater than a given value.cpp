0fd916496b567cc0a804a1118a741de84e31e954
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-01-28 13:26:38 +0530

    Smallest Subarray with greater sum (half).

---
+++ Array/Smallest Subarray with sum greater than a given value.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+
+// Function to find the size of smallest subarray whose sum is greater than a given value.
+
+// 1) If numbers are positive only :
+
+int smallestSubWithSum(int arr[],int n,int x){
+    
+    int currSum=0,end=0,start=0,minLen=INT_MAX;
+    while(end < n){
+        while(currSum <= x && end < n)
+            currSum+=arr[end++];
+
+        while(currSum > x && start < n){
+            if(end - start < minLen)
+                minLen=end - start;
+            
+            currSum-=arr[start++];
+        }
+    }
+
+    return minLen;
+}
+
+
+// 2) To handle negative numbers as well :
+
+
+int smallestSubWithSum2(int arr[],int n,int x){
+
+    
+}
+
+
+int main(){
+    int n,x;
+    cin>>n>>x;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    cout<<smallestSubWithSum(arr,n,x);
+    return 0;
+}
