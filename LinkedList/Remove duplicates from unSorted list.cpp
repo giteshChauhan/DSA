3eb037305cbe1f873af9e949f7520e92235bbc65
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-07-21 17:49:23 +0530

    Remove duplicates from unSorted list

--- LinkedList/Remove duplicates from unSorted list.cpp
+++ LinkedList/Remove duplicates from unSorted list.cpp
@@ -1,79 +1,89 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<sstream>
-#include<map>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_set>
 using namespace std;
 
-
-struct Node{
+struct Node
+{
     int data;
     Node *next;
-    Node(int dt){
-        data=dt;
-        next=NULL;
+    Node(int dt)
+    {
+        data = dt;
+        next = NULL;
     }
 };
 
-Node* removeDuplicates(Node*);
+Node *removeDuplicates(Node *);
 
-Node* buildList(string s){
-    if(s.length() == 0)
+Node *buildList(string s)
+{
+    if (s.length() == 0)
         return NULL;
-    
-    vector<string>v;
+
+    vector<string> v;
     istringstream strm(s);
-    for(string str;strm>>str;)
+    for (string str; strm >> str;)
         v.push_back(str);
-    
-    Node *head=new Node(stoi(v[0]));
-    Node *tail=head;
-    for(int i=1;i<v.size();i++){
-        tail->next=new Node(stoi(v[i]));
-        tail=tail->next;
+
+    Node *head = new Node(stoi(v[0]));
+    Node *tail = head;
+    for (int i = 1; i < v.size(); i++)
+    {
+        tail->next = new Node(stoi(v[i]));
+        tail = tail->next;
     }
     return head;
 }
 
-void printList(Node *head){
-    while(head){
-        cout<<head->data<<" ";
-        head=head->next;
+void printList(Node *head)
+{
+    while (head)
+    {
+        cout << head->data << " ";
+        head = head->next;
     }
 }
 
-int main(){
+int main()
+{
 
     string s;
-    getline(cin,s);
-    Node *head=buildList(s);
-    head=removeDuplicates(head);
+    getline(cin, s);
+    Node *head = buildList(s);
+    head = removeDuplicates(head);
     printList(head);
 
     return 0;
 }
 
-
 // Function to remove duplicates from an Un-sorted LinkedList
 
-Node* removeDuplicates(Node *head){
+Node *removeDuplicates(Node *head)
+{
 
-    if(!head || !head->next)
-        return NULL;
+    if (!head || !head->next)
+        return head;
 
-    Node *curr=head,*prev=NULL;
-    map<int,bool>mp;
-    mp[curr->data]=1;
-    while(curr->next){
-        prev=curr;
-        curr=curr->next;
-        if(mp.find(curr->data) != mp.end()){
-            Node *del=curr;
-            curr=prev;
-            prev->next=del->next;
-            delete del;
-        }else
-            mp[curr->data]=1;
+    unordered_set<int> seen;
+    Node *curr = head, *prev = NULL;
+
+    while (curr != NULL)
+    {
+
+        if (seen.find(curr->data) != seen.end())
+        {
+            prev->next = curr->next;
+            delete (curr);
+        }
+        else
+        {
+            seen.insert(curr->data);
+            prev = curr;
+        }
+        curr = prev->next;
     }
 
     return head;
