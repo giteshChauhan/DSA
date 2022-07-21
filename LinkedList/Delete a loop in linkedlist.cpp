d395c361b8203e729395d3704fd4a43834be1d9b
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-07-21 16:44:50 +0530

    Delete a loop in a linkedList

--- LinkedList/Delete a loop in linkedlist.cpp
+++ LinkedList/Delete a loop in linkedlist.cpp
@@ -1,47 +1,53 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
     Node *next;
-    Node(int dt){
-        data=dt;
-        next=NULL;
+    Node(int dt)
+    {
+        data = dt;
+        next = NULL;
     }
 };
 
 // Function to delete a loop from the linked list.
 
-void removeLoop(Node *head){
-    if(!head)
+void removeLoop(Node *head)
+{
+    if (!head)
         return;
 
-    Node *p1=head,*p2=head;
-    while(p1->next && p1->next->next){
-        p1=p1->next->next;
-        p2=p2->next;
-        if(p1 == p2)
-            break;
+    Node *p1 = head->next, *p2 = head;
+    while (p1 != p2)
+    {
+        p1 = p1->next->next;
+        p2 = p2->next;
+        if (!p1 || !p1->next)
+            return;
     }
 
-    int n=1;
-    while(p2->next == p1){
-        p2=p2->next;
+    int n = 1;
+    while (p2->next != p1)
+    {
+        p2 = p2->next;
         n++;
     }
-    p1=head;
+    p1 = head;
 
-    p2=head;
-    for(int i=0;i<n;i++)
-        p2=p2->next;
+    p2 = head;
+    for (int i = 0; i < n; i++)
+        p2 = p2->next;
 
-    while(p1!=p2){
-        p1=p1->next;
-        p2=p2->next;
-    }    
+    while (p1 != p2)
+    {
+        p1 = p1->next;
+        p2 = p2->next;
+    }
 
-    while(p2->next != p1)
-        p2=p2->next;
+    while (p2->next != p1)
+        p2 = p2->next;
 
-    p2->next=NULL;
+    p2->next = NULL;
 }
