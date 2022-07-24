703b1deeb6bac2caba3110ab89731c822b94a19a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-07-25 02:44:54 +0530

    Rotate DLL in group of given size | Added my method

--- LinkedList/Rotate DLL in group of given size.cpp
+++ LinkedList/Rotate DLL in group of given size.cpp
@@ -1,63 +1,97 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    Node *next,*prev;
-    Node(int dt){
-        data=dt;
-        next=NULL;
-        prev=NULL;
+    Node *next, *prev;
+    Node(int dt)
+    {
+        data = dt;
+        next = NULL;
+        prev = NULL;
     }
 };
 
-Node *buildList(string s){
-    if(s.length() == 0)
+Node *buildList(string s)
+{
+    if (s.length() == 0)
         return NULL;
-    
-    vector<string>v;
+
+    vector<string> v;
     istringstream strm(s);
-    for(string str;strm>>str;)
+    for (string str; strm >> str;)
         v.push_back(str);
-    
-    Node *head=new Node(stoi(v[0]));
-    Node *tail=head;
-    for(int i=1;i<v.size();i++){
-        tail->next=new Node(stoi(v[i]));
-        tail->next->prev=tail;
-        tail=tail->next;
+
+    Node *head = new Node(stoi(v[0]));
+    Node *tail = head;
+    for (int i = 1; i < v.size(); i++)
+    {
+        tail->next = new Node(stoi(v[i]));
+        tail->next->prev = tail;
+        tail = tail->next;
     }
     return head;
 }
 
-Node *revListInGroupOfGivenSize(Node*,int);
+Node *revListInGroupOfGivenSize(Node *, int);
 
-void printList(Node *head){
-    while(head){
-        cout<<head->data<<" ";
-        head=head->next;
+void printList(Node *head)
+{
+    while (head)
+    {
+        cout << head->data << " ";
+        head = head->next;
     }
 }
 
-int main(){
+int main()
+{
 
     string s;
-    getline(cin,s);
-    Node *head=buildList(s);
+    getline(cin, s);
+    Node *head = buildList(s);
     int N;
-    cin>>N;
-    head=revListInGroupOfGivenSize(head,N);
+    cin >> N;
+    head = revListInGroupOfGivenSize(head, N);
     printList(head);
 
     return 0;
 }
 
-
 // Function to reverse the doubly linked list in the group of given size
 
+// METHOD - 1 (Mine)
+
+Node *revListInGroupOfGivenSize(Node *head, int k)
+{
+
+    if (!head)
+        return head;
+
+    Node *prev = NULL, *cnext = NULL, *curr = head;
+    for (int i = 0; i < k && curr; i++)
+    {
+        cnext = curr->next;
+        curr->next = prev;
+        curr->prev = cnext;
+        prev = curr;
+        curr = cnext;
+    }
+    prev->prev = NULL;
+    Node *newHead = revListInGroupOfGivenSize(cnext, k);
+    head->next = newHead;
+    if (newHead)
+        newHead->prev = head;
+
+    return prev;
+}
+
+// METHOD - 2 (GFG)
+/*
 Node *revListInGroupOfGivenSize(Node *head,int k){
 
     if(!head)
@@ -83,3 +117,4 @@ Node *revListInGroupOfGivenSize(Node *head,int k){
 
     return newHead;
 }
+*/
