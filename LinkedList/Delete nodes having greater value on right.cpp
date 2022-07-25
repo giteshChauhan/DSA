e1676ac615c0204d14a5bbc464b1a3c76667fbb8
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-07-26 01:20:22 +0530

    Delete nodes having greater value on right | updated

--- LinkedList/Delete nodes having greater value on right.cpp
+++ LinkedList/Delete nodes having greater value on right.cpp
@@ -1,97 +1,110 @@
-#include<iostream>
-#include<vector>
-#include<sstream>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
     Node *next;
-    Node(int dt){
-        data=dt;
-        next=NULL;
+    Node(int dt)
+    {
+        data = dt;
+        next = NULL;
     }
 };
 
-Node *buildList(string s){
-    if(s.length()==0)
+Node *buildList(string s)
+{
+    if (s.length() == 0)
         return NULL;
-    
-    vector<string>v;
+
+    vector<string> v;
     istringstream strm(s);
-    for(string str;strm>>str;)
+    for (string str; strm >> str;)
         v.push_back(str);
-    
-    Node *head=new Node(stoi(v[0]));
-    Node *tail=head;
-    for(int i=1;i<v.size();i++){
-        tail->next=new Node(stoi(v[i]));
-        tail=tail->next;
+
+    Node *head = new Node(stoi(v[0]));
+    Node *tail = head;
+    for (int i = 1; i < v.size(); i++)
+    {
+        tail->next = new Node(stoi(v[i]));
+        tail = tail->next;
     }
 
     return head;
 }
 
-void printList(Node *head){
-    while(head){
-        cout<<head->data<<" ";
-        head=head->next;
+void printList(Node *head)
+{
+    while (head)
+    {
+        cout << head->data << " ";
+        head = head->next;
     }
 }
 
-Node *compute(Node*);
+Node *compute(Node *);
 
-int main(){
+int main()
+{
 
     string s;
-    getline(cin,s);
-    Node *head=buildList(s);
-    head=compute(head);
+    getline(cin, s);
+    Node *head = buildList(s);
+    head = compute(head);
     printList(head);
 
     return 0;
 }
 
-// Function to delete nodes heaving greater value on right 
+// Function to delete nodes heaving greater value on right
 
-Node *reverse(Node *head){
+Node *reverse(Node *head)
+{
 
-    if(!head || !head->next)
+    if (!head || !head->next)
         return head;
 
-    Node *cnext=NULL,*prev=NULL;
-    while(head){
-        cnext=head->next;
-        head->next=prev;
-        prev=head;
-        head=cnext;
+    Node *cnext = NULL, *prev = NULL;
+    while (head)
+    {
+        cnext = head->next;
+        head->next = prev;
+        prev = head;
+        head = cnext;
     }
 
     return prev;
 }
 
-Node *compute(Node *head){
+Node *compute(Node *head)
+{
 
-    if(!head || !head->next)
+    if (!head || !head->next)
         return head;
-    
-    head=reverse(head);
 
-    Node *it=head,*prev=NULL;
-    int maxVal=head->data;
+    head = reverse(head);
 
-    while(it->next){
-        prev=it;
-        it=it->next;
-        if(maxVal <= it->data)
-            maxVal=it->data;
-        else{
-            Node *del=it;
-            prev->next=it->next;
-            it=prev;
-            delete del;
+    head = reverse(head);
+    Node *prev = NULL, *curr = head;
+    int max = INT_MIN;
+
+    while (curr)
+    {
+        if (curr->data >= max)
+        {
+            max = curr->data;
+            prev = curr;
         }
+        else
+        {
+            prev->next = curr->next;
+            delete curr;
+        }
+        curr = prev->next;
     }
-    
-    return(reverse(head));
+
+    return (reverse(head));
 }
