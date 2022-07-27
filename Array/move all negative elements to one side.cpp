4fc384f357a05fb521a1372aa50367601ceb1301
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-07-28 02:07:00 +0530

    move all neg elements to last | order maintained

--- Array/move all negative elements to one side.cpp
+++ Array/move all negative elements to one side.cpp
@@ -1,37 +1,53 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
+// Function to move all negative elements to the last of an array.
+// Note : Order of elements should be  maintained
 
-// Function to move all negative elements to one side of an array.
-// Note : Order of elements is not maintained
-
-void swap(int &i,int &j){
-    int temp=i;
-    i=j;
-    j=temp;
+void swap(int &i, int &j)
+{
+    int temp = i;
+    i = j;
+    j = temp;
 }
 
-void rearrange(int arr[],int n){
-    int j=0;
-    for(int i=0;i < n;i++){
-        if(arr[i] < 0){
-            if(i != j)
-                swap(arr[i],arr[j]);
+void rearrange(int arr[], int n)
+{
+    int j = 0;
+    queue<int> q;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            swap(arr[i], arr[j]);
             j++;
         }
+        else
+        {
+            q.push(arr[i]);
+        }
+    }
+
+    while (j < n)
+    {
+        arr[j] = q.front();
+        q.pop();
     }
 }
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    rearrange(arr,n);
-    for(auto i:arr)
-        cout<<i<<" ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    rearrange(arr, n);
+    for (auto i : arr)
+        cout << i << " ";
 
     return 0;
 }
